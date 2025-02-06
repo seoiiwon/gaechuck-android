c6544ceb531481fca6af852c6b41513cb3c36eb8
JH <jhk3261@gnu.ac.kr>
2025-02-06 21:00:44 +0900

    ✨ feat: LoseWriteActivity 구현

---
+++ app/src/main/java/com/example/gaechuck/ui/lose/LoseWriteActivity.kt
@@ -0,0 +1,37 @@
+package com.example.gaechuck.ui.lose
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gaechuck.R
+import com.example.gaechuck.databinding.ActivityLoseWriteBinding
+
+class LoseWriteActivity : AppCompatActivity(R.layout.activity_lose_write) {
+
+    private lateinit var toolbar : androidx.appcompat.widget.Toolbar
+    private lateinit var backButton : ImageView
+    private lateinit var sendButton : TextView
+    private lateinit var binding: ActivityLoseWriteBinding
+    private lateinit var photoCountTextView: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoseWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // photo_count TextView 찾기
+        photoCountTextView = binding.photoAddBtn.photoCount
+        photoCountTextView.text = "3"
+
+        // Toolbar 설정
+        toolbar = findViewById(R.id.toolbar_main)
+        backButton = toolbar.findViewById(R.id.button_back)
+        sendButton = toolbar.findViewById(R.id.form_send)
+
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
