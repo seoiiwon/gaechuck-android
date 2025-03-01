fd0192fcd43f1f501ec5efd73f901946c564b5fa
JH <jhk3261@gnu.ac.kr>
2025-03-01 20:13:10 +0900

    ✨ feat : 수정/삭제 팝업 메뉴 추가

--- app/src/main/java/com/example/gaechuck/ui/lose/LoseActivity.kt
+++ app/src/main/java/com/example/gaechuck/ui/lose/LoseActivity.kt
@@ -2,9 +2,14 @@ package com.example.gaechuck.ui.lose
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.Gravity
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -52,6 +57,63 @@ class LoseActivity : AppCompatActivity(R.layout.activity_lose) {
             startActivity(intent)
             finish() // RentActivity 종료
         }
+
+        etcButton.setOnClickListener{
+            // 팝업 메뉴를 생성하여 '수정'과 '삭제' 옵션 추가
+            val themeWrapper = ContextThemeWrapper(this , R.style.PopupMenuStyle)
+            val popupMenu = PopupMenu(themeWrapper , etcButton, Gravity.END , 0 , R.style.PopupMenuStyle)
+            val menuInflater = popupMenu.menuInflater
+            menuInflater.inflate(R.menu.etc_menu, popupMenu.menu)
+
+            // 메뉴 항목 클릭 리스너
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_edit -> {
+                        // 수정 로직
+                        true
+                    }
+                    R.id.menu_delete -> {
+                        // 삭제 확인 다이얼로그
+                        showDeleteConfirmationDialog()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+
+        }
+
+    }
+
+    private fun showDeleteConfirmationDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.alert_detail_popup, null)
+
+        // 커스텀 다이얼로그 생성
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("삭제 확인")
+            .setMessage("정말 삭제하시겠습니까?")
+            .setView(dialogView) // 커스텀 레이아웃 설정
+            .create()
+
+
+        // 버튼 동작 설정
+        val positiveButton = dialogView.findViewById<Button>(R.id.dialog_yes_btn)
+        val negativeButton = dialogView.findViewById<Button>(R.id.dialog_no_btn)
+
+        positiveButton.setOnClickListener {
+            // 확인 버튼 클릭 시 삭제 처리
+            // deleteBusinessItem(businessItemId)
+            dialog.dismiss()
+        }
+
+        negativeButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.window?.setBackgroundDrawableResource(R.drawable.custom_popup_background)
+        dialog.show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
