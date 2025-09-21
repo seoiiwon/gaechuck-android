0df98ff5455218372536c4b86e8d8e8603f4c5f7
JH <jhk3261@gnu.ac.kr>
2025-09-21 20:20:33 +0900

    💄 style[#100] : 다크모드 통일

--- app/src/main/java/com/gaechuck_package/gaechuck/Gaechuck.kt
+++ app/src/main/java/com/gaechuck_package/gaechuck/Gaechuck.kt
@@ -1,12 +1,14 @@
 package com.gaechuck_package.gaechuck
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.gaechuck_package.gaechuck.api.ApiConnection
 import com.gaechuck_package.gaechuck.api.AuthManager
 
 class Gaechuck : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         AuthManager.init(this)
         ApiConnection.create(this)
     }
